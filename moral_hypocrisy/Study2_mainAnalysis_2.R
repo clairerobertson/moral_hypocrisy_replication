07a53c2cf4ba969b6dc58e8d4846fb8c8c106b06
clairerobertson <cer493@nyu.edu>
2023-12-18 17:52:12 -0500

    Update Study2_mainAnalysis_2.R

--- moral_hypocrisy/Study2_mainAnalysis_2.R
+++ moral_hypocrisy/Study2_mainAnalysis_2.R
@@ -163,25 +163,79 @@ model4_data %>% filter(condition==4) %>%
 compare_means(fairness ~ condition.f, data = data_subset)
 my_comparisons <- list(c("ingroup", "outgroup")) # c("self", "outgroup"), c("other", "outgroup"))
 
-## Overeall Mean Fairnes Graph
 p <- ggplot(data_subset, aes(x = condition.f, y = fairness, fill = condition.f)) + 
   geom_violin(alpha = .2) + 
   scale_x_discrete(limits = c("self", "other", "ingroup", "outgroup"), 
-                   labels = c("Self", "Other", "Ingroup", "Outgroup")) + 
-  geom_jitter(aes(colour = condition.f), position = position_jitter(width = 0.2, height = 0.4), alpha = 0.4) + 
+                   labels = c("Self", "Other", "Ingroup", "Outgroup")) +
+  scale_y_continuous(breaks = c(1,2,3,4,5,6,7)) + 
+  geom_jitter(position = position_jitter(width = 0.25, height = 0.25), alpha = 0.4) + 
   stat_summary(fun.data = "mean_cl_boot", geom = "errorbar", size = 1, color = "black", alpha = 0.8) +
   stat_summary(fun.data = "mean_cl_boot", geom = "point", size = 2, color = "black", alpha = 0.8) + 
-  stat_compare_means(comparisons = my_comparisons) + 
+  geom_segment(x = 2.8, xend = 4.2, y = 7.3, yend = 7.3) + 
+  annotate("text", x = 3.5, y = 7.6, label = "**", size = 16/.pt) + 
   scale_fill_brewer(palette = "Dark2", labels = c("Self", "Other", "Ingroup", "Outgroup")) + 
-  scale_color_brewer(palette = "Dark2", labels = c("Self", "Other", "Ingroup", "Outgroup")) + 
   theme_bw()
- 
+p
+
 ## Graph Labeling
-p + labs(title = "Average Fairness Ratings",
-         subtitle = "Altruists Excluded",
-         x = "Condition",
-         y = "Fairness Ratings", 
-         color = "Condition",
-         fill = "Condition") + 
+p <- p + labs(title = "Altruists Excluded",
+              x = "Condition",
+              y = "Fairness Ratings", 
+              color = "Condition",
+              fill = "Condition") + 
   theme(legend.position = "none") # Can remove if you want the legend back
+p
+
+
+#### ALTRUISTS INCLUDED ####
+data <- data %>% 
+  mutate(altruists = ifelse(redgreen_selection == 2 | cond1_selection == 2, "fair", NA)) %>% 
+  mutate(altruists = ifelse(is.na(altruists), "unfair", "fair"))
+
+## Overeall Mean Fairnes Graph
+p2 <- ggplot(data, aes(x = condition.f, y = fairness, fill = condition.f)) + 
+  geom_violin(alpha = .2) + 
+  scale_x_discrete(limits = c("self", "other", "ingroup", "outgroup"), 
+                   labels = c("Self", "Other", "Ingroup", "Outgroup")) +
+  scale_y_continuous(breaks = c(1,2,3,4,5,6,7)) + 
+  geom_jitter(aes(colour = altruists, fill = condition.f), position = position_jitter(width = 0.25, height = 0.25), alpha = 0.4) +
+  scale_color_manual(values = c("fair" = "red", "unfair" = "black")) +
+  stat_summary(fun.data = "mean_cl_boot", geom = "errorbar", size = 1, color = "black", alpha = 0.8) +
+  stat_summary(fun.data = "mean_cl_boot", geom = "point", size = 2, color = "black", alpha = 0.8) + 
+  geom_segment(x = 2.8, xend = 4.2, y = 7.3, yend = 7.3) + 
+  annotate("text", x = 1, y = 7.6, label = "***", size = 16/.pt) +
+  annotate("text", x = 3.5, y = 7.6, label = "**", size = 16/.pt) + 
+  scale_fill_brewer(palette = "Dark2", labels = c("Self", "Other", "Ingroup", "Outgroup")) + 
+  theme_bw()
+
+## Graph Labeling
+p2 <- p2 + labs(title = "Altruists Included (in Red)",
+                x = "Condition",
+                y = "Fairness Ratings", 
+                color = "Condition",
+                fill = "Condition") + 
+  theme(legend.position = "none") # Can remove if you want the legend back
+p2
+
+
+## Graph Labeling
+p2 <- p2 + labs(title = "Altruists Included (in Red)",
+                x = "Condition",
+                y = "Fairness Ratings", 
+                color = "Condition",
+                fill = "Condition") + 
+  theme(legend.position = "none") # Can remove if you want the legend back
+p2
+
+plot <- ggarrange(p, p2, 
+                  labels = c("A.", "B.")) 
+
+plot <- annotate_figure(plot, top = text_grob("Fairness Ratings in Study 2, Natural Groups", 
+                                              color = "black", size = 14))
+
+plot
+
+ggsave("Plots/Study2_fairness.png", width = 3000, height = 1500, units = "px", scale = 1)
+
+
 
